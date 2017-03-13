d205afc5d7ab3bbd982f14287dcc5a46d25e90e7
eric glassman <eglassman@scitent.com>
2017-03-13 16:23:19 -0400

    adds actionplan

--- example.js
+++ example.js
@@ -6,6 +6,7 @@ import { bus } from 'partybus';
 //////////////////////////////////////////////////
 
 bus.on('tincan::ready', function(){
+  console.log('tincan::ready send: ', 'start, storeAttemptState, complete');
   bus.emit('tincan::start');
   bus.emit('tincan::storeAttemptState', { completionState: {} });
   bus.emit('tincan::complete');
@@ -45,16 +46,22 @@ import tincanInit from './src/tincan';
 // test real use //
 ///////////////////
 
-var tincan = tincanInit(config, isLocal);
+//var tincan = tincanInit(config, isLocal);
 
 //////////////////////////////
 // test demo/production use //
 //////////////////////////////
 
-//var authUrl = 'https://17515-presscdn-0-77-pagely.netdna-ssl.com/wp-content/uploads/grassblade/6896-course-3/index.html?actor=%7B%22mbox%22%3A%22mailto%3Atjohnson%40scitent.us%22%2C%22name%22%3A%22Tim%20Johnson%22%2C%22objectType%22%3A%22Agent%22%7D&auth=Basic%20MzctMzRiOTNjYWI2MTc0MmUwOmRjOThhYjFjN2U3NDRmZTE4NGRkYzU4N2U%3D&endpoint=https%3A%2F%2Flrs.scitent.us%2FxAPI%2F&registration=&activity_id=http%3A%2F%2Fcareerdevelopment.aaas.org%2Fcourse3';
-//var tincan = tincanInit(config, false, authUrl);
+var email = 'xftohnson';
+var firstName = 'Gim';
+var lastName = 'Tohnson';
 
-// EXAMPLES:
+var authUrl = `https://17515-presscdn-0-77-pagely.netdna-ssl.com/wp-content/uploads/grassblade/6896-course-3/index.html?actor=%7B%22mbox%22%3A%22mailto%3A${email}%40scitent.us%22%2C%22name%22%3A%22${firstName}%20${lastName}%22%2C%22objectType%22%3A%22Agent%22%7D&auth=Basic%20MzctMzRiOTNjYWI2MTc0MmUwOmRjOThhYjFjN2U3NDRmZTE4NGRkYzU4N2U%3D&endpoint=https%3A%2F%2Flrs.scitent.us%2FxAPI%2F&registration=&activity_id=http%3A%2F%2Fcareerdevelopment.aaas.org%2Fcourse3`;
+var tincan = tincanInit(config, false, authUrl);
+
+///////////////
+// EXAMPLES: //
+///////////////
 
 $(document).on('click', '#start', function(){
   tincan.start().tap(console.log).drain();
@@ -92,4 +99,28 @@ $(document).on('click', '#get-poll-response', function(){
 $(document).on('click', '#get-poll-responses', function(){
   var identifier = 'Poll 1';
   tincan.getPollResponsesData({ identifier }).tap(console.log).drain();
+});
+
+$(document).on('click', '#actionplan-update', function(){
+  var actionplan = {
+    'ap-1': {
+      'enter-ten-items': {
+        text: 'Enter 10 items...',
+        context: ['Section 1', 1489421176326],
+        value: '1. hi, 2. ok, 3. bye, ....'
+      },
+      'what-are-your-thoughts': {
+        text: 'What are your thoughts?',
+        context: ['Section 2', 1589421176327],
+        value: 'I have a few...'
+      }
+    }
+  };
+
+  tincan.setActionplanData({ takeaway: actionplan }).tap(console.log).drain();
+});
+
+$(document).on('click', '#get-actionplan', function(){
+  var actionplanId = 'ap-1';
+  tincan.getActionplanData({ identifier: actionplanId }).tap(console.log).drain();
 });
--- src/tincan.js
+++ src/tincan.js
@@ -1,6 +1,7 @@
 import $ from 'jquery'; // TODO: need to get rid of this after fixing resume progress
 import { timer, slugify } from './helpers';
 import { bus } from 'partybus';
+import R from 'ramda';
 import * as most from 'most';
 import { async } from 'most-subject';
 import TinCan from 'tincanjs';
@@ -763,12 +764,124 @@ export default function(CONFIG, isLocal, authUrl){
     return subject;
   };
 
+  ////////////////
+  // ACTIONPLAN //
+  ////////////////
+
+  // action plan type action
+  var actionPlanVerb = 'http://adlnet.gov/expapi/verbs/actionPlanUpdated';
+  var actionPlanDisplay = 'updated action plan';
+
+  // action plan identifier
+  var actionPlanIdExtension = 'http://adlnet.gov/expapi/activities/actionPlan';
+  var actionPlanResponsesExtension = 'http://adlnet.gov/expapi/activities/actionPlanResponses';
+
+  const setActionplanData = function({ takeaway }){
+    // takeaway :: { identifier: { 'qId': { text: '', context: [], value: '' }, 'qId2': { ... }, ... } }
+
+    var identifier = R.compose(R.head, R.keys)(takeaway);
+    var responses = R.compose(R.head, R.values)(takeaway);
+
+    const subject = async();
+
+    var statements = [];
+
+    var createObject = function () {
+      return {
+        "id": TC_COURSE_ID,
+        "objectType": "Activity",
+        "definition": {
+          "name": {"en-US": identifier },
+          "description": {"en-US": TC_COURSE_NAME["en-US"]},
+          "type": actionPlanIdExtension
+        },
+        "name": identifier
+      }
+    };
+
+    var createResult = function () {
+      return {}
+    };
+
+    var context = {};
+
+    var extensions = {};
+
+    extensions[actionPlanIdExtension] = identifier;
+
+    extensions[actionPlanResponsesExtension] = responses;
+
+    context['extensions'] = extensions;
+
+    var interactionStatement = {
+      verb: {
+        id: actionPlanVerb,
+        display: {
+          "en-US": actionPlanDisplay
+        }
+      },
+      result: createResult(),
+      object: createObject(),
+      context: context
+    };
+
+    statements.push(interactionStatement);
+
+    tincan.sendStatements(statements, function(response, data){
+      var err = response.err;
+      if(err){ subject.error(err).complete(); } else { subject.next(data).complete(); }
+    });
+
+    return subject;
+  };
+
+  const getActionplanData = function({ identifier }){
+    const subject = async();
+
+    //TODO: extract AJAX into function that takes params and builds query string like `tincan.getStatements`...
+
+    var query = '?activity=' + tincan.activity.id + '&verb=' + actionPlanVerb;
+
+    var handleActionplanData = function(result){
+      var actionplanId = identifier;
+      var thisActor = function(statement) {
+        return statement['actor']['account'] ? (statement['actor']['account']['name'] === tincan.actor.account.name) : (statement['actor']['mbox'] === tincan.actor.mbox)
+      };
+      var statements = result['statements'].filter(thisActor).filter(function(statement) {
+        return statement['context']['extensions'] && statement['context']['extensions'][actionPlanIdExtension] === actionplanId
+      }).sort(function(a,b){
+        // Turn your strings into dates, and then subtract them
+        // to get a value that is either negative, positive, or zero.
+        return new Date(b.timestamp) - new Date(a.timestamp);
+      });
+
+      var recentStatement = R.head(statements);
+
+      var id = R.pathOr(null, ['context', 'extensions', actionPlanIdExtension], recentStatement);
+      var body = R.pathOr({}, ['context', 'extensions', actionPlanResponsesExtension], recentStatement);
+
+      subject.next(id ? { [id]: body }: {}).complete();
+
+    };
+
+    $.ajax({
+      url: tincan.recordStores[0]['endpoint'] + "statements" + query,
+      type: "GET",
+      beforeSend: function (xhr) {
+        xhr.setRequestHeader('Authorization', tincan.recordStores[0]['auth']);
+      },
+      success: handleActionplanData
+    });
+
+    return subject;
+  };
+
   /////////
   // API //
   /////////
 
   const local = function(methodString){
-    return (params) => most.just({ [methodString]: params }).tap(console.log);
+    return (params) => most.just({ [methodString]: params }).tap((result) => { console.log('local', result) });
   };
 
   const api = {
@@ -784,7 +897,9 @@ export default function(CONFIG, isLocal, authUrl){
     accessHelp: isLocal ? local('☂ accessHelp:') : accessHelp,
     setPollResponse: isLocal ? local('☂ setPollResponse') : setPollResponse,
     getPollResponseData: isLocal ? local('☂ getPollResponseData') : getPollResponseData,
-    getPollResponsesData: isLocal ? local('☂ getPollResponsesData') : getPollResponsesData
+    getPollResponsesData: isLocal ? local('☂ getPollResponsesData') : getPollResponsesData,
+    setActionplanData: isLocal ? local('☂ setActionplanData') : setActionplanData,
+    getActionplanData: isLocal ? local('☂ getActionplanData') : getActionplanData
   };
 
   bus.on('tincan::start', () => { api.start().drain(); });
